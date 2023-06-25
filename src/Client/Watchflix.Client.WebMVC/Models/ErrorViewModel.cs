f137c89eba3001fa086c2213bf4e4469b63ecef4
Baran Yasar <byasarn@gmail.com>
2023-06-25 11:51:51 +0300

    Add project files.

---
+++ src/Client/Watchflix.Client.WebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Watchflix.Client.WebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
